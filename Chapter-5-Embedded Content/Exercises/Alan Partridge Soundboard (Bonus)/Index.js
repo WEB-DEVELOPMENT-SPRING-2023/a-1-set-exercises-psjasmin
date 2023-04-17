589c569762e5ee888f738840a51ba354852fd700
Princess Jasmin Panilagao <113695942+psjasmin@users.noreply.github.com>
2023-04-17 14:27:36 +0400

    Create Index.js

---
+++ Chapter-5-Embedded Content/Exercises/Alan Partridge Soundboard (Bonus)/Index.js
@@ -0,0 +1,53 @@
+//Ah-Ha
+function aha(){
+    var snd = new Audio('https://www.dropbox.com/s/atbwxbpryfcttpk/ah-ha.mp3?dl=1') //audio link
+    snd.play()//plays the sound
+}
+
+//Dan
+function dan(){
+  var snd = new Audio('https://www.dropbox.com/s/1dbnppznxwr15ih/dan.mp3?dl=1')
+  snd.play()
+}
+
+//Bang of the Net
+function botn(){
+  var snd = new Audio('https://www.dropbox.com/s/p0un23b0k5az2a5/back-of-the-net.mp3?dl=1')
+  snd.play()
+}
+
+//Bang Out of Order
+function booo(){
+  var snd = new Audio('https://www.dropbox.com/s/uu2jk82q6nsg3ju/bangoutoforder.mp3?dl=1')
+  snd.play()
+}
+
+//Jurassic Park
+function jurassic(){
+  var snd = new Audio('https://www.dropbox.com/s/drutwih3vjpyh15/emailoftheevening.mp3?dl=1')
+  snd.play()
+}
+
+//Smell My Cheese
+function smc(){
+  var snd = new Audio('https://www.dropbox.com/s/gyhtodtxlf23czc/iateascotchegg.mp3?dl=1')
+  snd.play()
+}
+
+//Goal
+function goal(){
+  var snd = new Audio('')
+  snd.play()
+}
+
+//Kiss My Face
+function kiss(){
+  var snd = new Audio('https://www.dropbox.com/s/0xb9kwytzla9e97/imconfused.mp3?dl=1')
+  snd.play()
+}
+
+//Hello Partridge
+function hp(){
+  var snd = new Audio('https://www.dropbox.com/s/4v8d64mw8id5fog/hellopartridge.mp3?dl=1')
+  snd.play()
+}
